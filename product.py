33fc7bed2feb4f9988fd4bb22c3f39fcc1ccd3de
SREEVATHSAV KYAMA <98406156+SREEVATHSAVK@users.noreply.github.com>
2023-09-26 10:03:17 +0530

    Update product.py

--- product.py
+++ product.py
@@ -19,36 +19,36 @@ def connect_to_product():
         window = Toplevel()
         window.geometry("450x450+550+150")
         window.title(title)
-        window.config(bg="pink") 
+        window.config(bg="blanched almond") 
         window.resizable(0,0)
         window.grab_set()
 
-        idLabel = Label(window,text = 'Product ID',font=('arial',16,'bold'),compound=LEFT,bg="pink")
+        idLabel = Label(window,text = 'Product ID',font=('arial',16,'bold'),compound=LEFT,bg="blanched almond")
         idLabel.place(x=20, y=30)
         idEntry = Entry(window,font=('times new roman',13,'bold'),fg="black",bd=3)
         idEntry.place(x=220, y=32)
 
-        nameLabel = Label(window,text = 'Product Name',font=('arial',16,'bold'),compound=LEFT,bg="pink")
+        nameLabel = Label(window,text = 'Product Name',font=('arial',16,'bold'),compound=LEFT,bg="blanched almond")
         nameLabel.place(x=20, y=100)
         nameEntry = Entry(window,font=('times new roman',13,'bold'),fg="black",bd=3)
         nameEntry.place(x=220, y=102)
 
-        priceLabel = Label(window,text = 'Price',font=('arial',16,'bold'),compound=LEFT,bg="pink")
+        priceLabel = Label(window,text = 'Price',font=('arial',16,'bold'),compound=LEFT,bg="blanched almond")
         priceLabel.place(x=20, y=170)
         priceEntry = Entry(window,font=('times new roman',13,'bold'),fg="black",bd=3)
         priceEntry.place(x=220, y=172)
 
-        brandLabel = Label(window,text = 'Brand',font=('arial',16,'bold'),compound=LEFT,bg="pink")
+        brandLabel = Label(window,text = 'Brand',font=('arial',16,'bold'),compound=LEFT,bg="blanched almond")
         brandLabel.place(x=20, y=240)
         brandEntry = Entry(window,font=('times new roman',13,'bold'),fg="black",bd=3)
         brandEntry.place(x=220, y=242)
 
-        sectionLabel = Label(window,text = 'Section',font=('arial',16,'bold'),compound=LEFT,bg="pink")
+        sectionLabel = Label(window,text = 'Section',font=('arial',16,'bold'),compound=LEFT,bg="blanched almond")
         sectionLabel.place(x=20, y=310)
         sectionEntry = Entry(window,font=('times new roman',13,'bold'),fg="black",bd=3)
         sectionEntry.place(x=220, y=312)
 
-        searchButton = Button(window,command=command,text=text,width=15,font=('times new roman',14,'bold'),bg = "#3EB489",cursor="hand2")
+        searchButton = Button(window,command=command,text=text,width=15,font=('times new roman',14,'bold'),bg = "sandy brown",cursor="hand2")
         searchButton.place(x = 140, y = 370)
 
         if title == 'Update Operation':
@@ -83,14 +83,14 @@ def connect_to_product():
         window = Toplevel()
         window.geometry("450x200+540+280")
         window.title("Delete Operation")
-        window.config(bg="pink") 
+        window.config(bg="blanched almond") 
         window.resizable(0,0)
         window.grab_set()
 
-        display = Label(window,text='Did you select the row of product table ?\n  If not, Please select the row of table',font=('arial',15,'bold'),bg='pink')
+        display = Label(window,text='Did you select the row of product table ?\n  If not, Please select the row of table',font=('arial',15,'bold'),bg='blanched almond')
         display.place(x=20, y=30)
 
-        yesButton = Button(window,command=delete_data,text='YES',font=('arial',12,'bold'),width=6,bg='yellow',cursor="hand2")
+        yesButton = Button(window,command=delete_data,text='YES',font=('arial',12,'bold'),width=6,bg='sandy brown',cursor="hand2")
         yesButton.place(x=190,y=100)
 
     def search_data():
@@ -137,16 +137,16 @@ def connect_to_product():
     root = Toplevel()
     style = ttk.Style(root)
     style.theme_use("default")
-    root.geometry("1400x750+65+15")
+    root.geometry("1275x770-1+0")
     root.title("Product Table")
     root.config(bg='whitesmoke')
-    root.resizable(0,0)
+    # root.resizable(0,0)
 
     heading_label = Label(root,font=("Arial", 30,"bold"), pady=15,bg="whitesmoke")
     heading_label.pack()
 
     middleFrame = Frame(root, bg = "white",highlightbackground='black',highlightthickness=1)
-    middleFrame.place(x = 50,y = 100, width = 1300, height = 520)
+    middleFrame.place(x = 35,y = 100, width = 1200, height = 520)
 
     productTable = ttk.Treeview(middleFrame,columns=('prod_id','prod_name','price','brand','section'))
     productTable.pack(fill=BOTH,expand=1)
@@ -164,20 +164,20 @@ def connect_to_product():
 
     productTable.config(show='headings')
     view()
-    addButton = Button(root,command=lambda: operations('Add Operation','ADD DATA',add_data),text='Insert',width=10,font=('times new roman',16,"bold"),bg='#00FF00',cursor="hand2")
-    addButton.place(x=150,y=650)
+    addButton = Button(root,command=lambda: operations('Add Operation','ADD DATA',add_data),text='Insert',width=10,font=('times new roman',16,"bold"),bg='orange',cursor="hand2")
+    addButton.place(x=35,y=650)
 
-    searchButton = Button(root,command=lambda: operations('Serach Operation','SEARCH DATA',search_data),text='Search',width=10,font=('times new roman',16,"bold"),bg='#DA70D6',cursor="hand2")
-    searchButton.place(x=400,y=650)
+    searchButton = Button(root,command=lambda: operations('Serach Operation','SEARCH DATA',search_data),text='Search',width=10,font=('times new roman',16,"bold"),bg='khaki3',cursor="hand2")
+    searchButton.place(x=300,y=650)
 
-    viewButton = Button(root,command=view,text='View Data',width=10,font=('times new roman',16,"bold"),bg='yellow',cursor="hand2")
-    viewButton.place(x=650,y=650)
+    viewButton = Button(root,command=view,text='View Data',width=10,font=('times new roman',16,"bold"),bg='bisque3',cursor="hand2")
+    viewButton.place(x=560,y=650)
 
-    deleteButton = Button(root,command=delete,text='Delete',width=10,font=('times new roman',16,"bold"),bg='red',cursor="hand2")
-    deleteButton.place(x=900,y=650)
+    deleteButton = Button(root,command=delete,text='Delete',width=10,font=('times new roman',16,"bold"),bg='OrangeRed2',cursor="hand2")
+    deleteButton.place(x=840,y=650)
 
-    updateButton = Button(root,command=lambda: operations('Update Operation','UPDATE DATA',update_data),text='Update',width=10,font=('times new roman',16,"bold"),bg='#A020F0',cursor="hand2")
-    updateButton.place(x=1150,y=650)
+    updateButton = Button(root,command=lambda: operations('Update Operation','UPDATE DATA',update_data),text='Update',width=10,font=('times new roman',16,"bold"),bg='thistle1',cursor="hand2")
+    updateButton.place(x=1100,y=650)
 
     style = ttk.Style()
     style.configure('Treeview',rowheight=25,font=('arial',12,'normal'))
